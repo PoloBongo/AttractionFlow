966b3ed9bc6996074c506549fa5d04106cf49587
JulenMYT <147490703+JulenMYT@users.noreply.github.com>
2025-03-13 19:29:37 +0100

    Script
    DossierScript

---
+++ Assets/Scripts/OpenAttraction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAttraction : MonoBehaviour
+{
+    private bool isOpen = false;
+    void Open()
+    {
+        isOpen = true;
+        PlayOpenAnimation();
+    }
+
+    void Close()
+    {
+        isOpen = false;
+        PlayCloseAnimation();
+    }
+
+    void PlayOpenAnimation()
+    {
+
+    }
+
+    void PlayCloseAnimation()
+    {
+
+    }
+}

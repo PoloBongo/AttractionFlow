2ed9e206c8551aff368cb72a5ae7ad727ff934c8
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2025-03-13 23:41:37 +0100

    Added Triggers to give or remove cash

---
+++ Assets/Scripts/AttractionTrigger.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Type
+{
+    ENTER,
+    LEAVE,
+}
+
+public class AttractionTrigger : MonoBehaviour
+{
+    [SerializeField] private BoxCollider boxCollider;
+    [SerializeField] private MoneyManager moneyManager;
+    [SerializeField] private Type type;
+
+    private void Awake()
+    {
+        if (GetComponent<BoxCollider>() != null)
+        {
+            boxCollider = GetComponent<BoxCollider>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Character"))
+        {
+            Debug.Log("Character entered attraction trigger");
+            
+            if (type == Type.ENTER)
+            {
+                moneyManager.AddMoney(10);
+            }
+            else if (type == Type.LEAVE)
+            {
+                moneyManager.SubtractMoney(10);
+            }
+            
+        }
+    }
+}

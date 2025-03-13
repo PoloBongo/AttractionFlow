4f7f82073ce681fefee984a434515523bd84c13a
PoloBongo <earthrp.polo@gmail.com>
2025-03-14 00:07:25 +0100

    [ADD] assets & setup anims portal when touching an attraction

--- Assets/Scripts/OpenAttraction.cs
+++ Assets/Scripts/OpenAttraction.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class OpenAttraction : MonoBehaviour
 {
     private bool isOpen = false;
+    [Header("Animation")] [SerializeField] private Animator fenceAnimator;
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+
     void Open()
     {
-        isOpen = true;
+        fenceAnimator.SetBool(IsOpen, isOpen);
         PlayOpenAnimation();
+        
+        Debug.Log("Open Attraction");
     }
 
     void Close()
     {
-        isOpen = false;
+        fenceAnimator.SetBool(IsOpen, isOpen);
         PlayCloseAnimation();
+        
+        Debug.Log("Close Attraction");
+    }
+
+    public void InteractAttraction()
+    {
+        isOpen = !isOpen;
+        
+        if (isOpen)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+        
     }
 
     void PlayOpenAnimation()

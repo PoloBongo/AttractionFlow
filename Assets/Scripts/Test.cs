30c69ce9c4c050992d934baccee8a27f006ff29d
StratiX0 <115560299+StratiX0@users.noreply.github.com>
2025-03-14 02:15:50 +0100

    Add Camera Movement

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,20 +7,27 @@ using UnityEngine.InputSystem;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private InputActionReference pressActions;
     [Header("Input Actions")]
+    [SerializeField] private InputActionReference pressActions;
+    [SerializeField] private InputActionReference holdActions;
+    [SerializeField] private InputActionReference deltaActions;
     [SerializeField] private OpenAttraction selectedAttraction;
     [SerializeField] private OpenAttraction selectedAttractionParent;
     private Camera camera;
+    private bool isHolding = false;
     
     private void OnEnable()
     {
         pressActions.action.Enable();
+        holdActions.action.Enable();
+        deltaActions.action.Enable();
     }
     
     private void OnDisable()
     {
         pressActions.action.Disable();
+        holdActions.action.Disable();
+        deltaActions.action.Disable();
     }
 
     void Start()
@@ -30,7 +37,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void OnTouch(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && context.duration < 0.1f)
         {
             Vector2 touchPosition = context.ReadValue<Vector2>();
 
@@ -48,6 +55,28 @@ public class NewBehaviourScript : MonoBehaviour
                     Debug.Log("n'arrive pas a get le comp");
                 }
             }
+        }
+    }
+
+    public void OnHold(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isHolding = true;
+        }
+        else if (context.canceled)
+        {
+            isHolding = false;
+        }
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (isHolding && context.performed)
+        {
+            Vector2 delta = context.ReadValue<Vector2>();
+            camera.transform.position += new Vector3(-delta.x * Time.deltaTime * 3f, 0, -delta.y * Time.deltaTime * 3f);
+            camera.transform.position = new Vector3(Mathf.Clamp(camera.transform.position.x, -10, 10), camera.transform.position.y, Mathf.Clamp(camera.transform.position.z, -10, 10));
         }
     }
 }

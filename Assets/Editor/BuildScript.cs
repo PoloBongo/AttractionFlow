4d41009cccc5c8119d72cf826a2785e0a62b5734
Arthur <earthrp.polo@gmail.com>
2025-04-04 10:48:53 +0200

    [UPDATE] build test

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+    public static void BuildAndroid()
+    {
+        string buildPath = "builds/Android.apk";
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/Main.unity" }, // Change selon ton projet
+            locationPathName = buildPath,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (report.summary.result != UnityEditor.Build.Reporting.BuildResult.Succeeded)
+        {
+            Debug.LogError("Build failed!");
+        }
+        else
+        {
+            Debug.Log("Build succeeded!");
+        }
+    }
+}
